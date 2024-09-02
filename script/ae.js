f0d56263dcdc0a12b4db6bbf7987bd234e450bb2
Thea / Aesther <154717468+Josephstarlord@users.noreply.github.com>
2024-09-02 23:29:17 +0300

    Update ae.js

--- script/ae.js
+++ script/ae.js
@@ -33,7 +33,7 @@ module.exports.run = async function({ api, event, args }) {
     try {
         const RolePlay = "Répond à cette question avec une décore d'emoji convenable\n>";
         const { data } = await axios.get(`https://smfahim.onrender.com/hercai?ask=${encodeURIComponent(RolePlay + input)}`);
-        let response = data.response;
+        let response = data.answer;
         
         // Replace characters with stylized characters from fonts
         response = response.split('').map(char => fonts[char] || char).join('');
